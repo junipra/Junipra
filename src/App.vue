48c7d9a47a5d28032806420b37e20e314a0ca095
cameronbaffuto <info@junipra.com>
2025-01-04 13:10:24 -0500

    title clickable

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ import { RouterView } from 'vue-router';
       <div class="flex items-center space-x-4">
         <RouterLink to="/">
           <img src="/src/assets/Junipra_logo.svg" alt="Junipra Logo" class="h-16 w-16" />
+          <span class="text-3xl font-mystery text-gray-800">Junipra Innovations</span>
         </RouterLink>
-        <span class="text-3xl font-mystery text-gray-800">Junipra Innovations</span>
       </div>
 
       <nav class="flex space-x-4">
